7018ff5e1e74d60eaa3c0198adc07cc28094e72e
francoiscrtz <francoiscreutzer@gmail.com>
2023-11-27 16:36:12 +0100

    test 0 added

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,11 +1,28 @@
 const request = require("supertest");
 const app = require("../app");
 
-describe("GET /api/movies/1", () => {
-  it("should return movies/1", async () => {
+describe("Get /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("Get /api/movies/:id", () => {
+  it("should return one movie", async () => {
     const response = await request(app).get("/api/movies/1");
 
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
+  });
+});
+
+describe("Get /api/movies/:id", () => {
+  it("should return 404", async () => {
+    const response = await request(app).get("/api/movies/0");
+
+    expect(response.status).toEqual(404);
   });
 });
